b19fb86b4c685a23a1050e03a7d4efd0f3b496bc
Somenath Sit <somenath.bhu.2010@gmail.com>
2023-06-10 15:40:48 +0530

    text generation and translation
    Added Text generation and translation functionality

--- example.py
+++ example.py
@@ -1,15 +1,20 @@
 #pip install pyaikit
-
+import os
+os.chdir("C:/Users/Somenath/Desktop/Drive/Somenath/Code/PyAIKit/")
 from pyaikit import authentication, sentiment_analysis,text_summerization
+from pyaikit.authentication import Authenticator
 from pyaikit.sentiment_analysis.sentiment_analyzer import sentiment_analyzer
 from pyaikit.text_summerization.text_summerizer import text_summerizer
-from pyaikit.authentication import Authenticator
+from pyaikit.text_generation.text_generator import text_generator
+from pyaikit.translation.translator import translator
 
 auth = Authenticator()
 
 # Set up authentication with the API key
 api_key = 'YOUR_API_KEY'
 org_id = 'YOUR_org_id'
+api_key = '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'
+org_id = 'org-q8UHttO0m7Jqd7N3R9BFnu2h'
 openai1=auth.setup(api_key,org_id)
 
 #Sentiment analysis
@@ -20,3 +25,13 @@ obj.generate_advanced_sentiment(openai1,"text")
 #text summerizer
 obj=text_summerizer()
 obj.summarize_pdf(openai1, no_of_words="no of words in per page summary", file_path="pdf file path", start_page="start page number", end_page="end page number")
+
+
+#text generator
+obj=text_generator()
+obj.generate_text(openai1, topic="title of your blog", no_of_words="20")
+
+
+#text generator
+obj=translator()
+obj.translate_text(openai1, text="text you want to translate", language="French")
